f7206f19c9c0dcd1ee57711ba8bec54c10212ef7
Henrique Custódia <martinshenrique8@gmail.com>
2023-06-09 22:20:37 -0300

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [NgIf, NgFor],
+  template: `
+    <h1>I'm just an example</h1>
+
+    <div *ngIf="showText">Hi, I'm a text</div>
+
+    <button (click)="onClick()">Toggle</button>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        border: 1px solid red;
+      }
+    `,
+  ],
+})
+export class AppExample {
+  showText = false;
+
+  onClick() {
+    this.showText = !this.showText;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [CommonModule, AppExample],
+  template: ` <app-example /> `,
+  styles: [
+    `
+      :host {
+        display: block;
+        border: 1px solid blue;
+        padding: 16px;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'standalone-components-example';
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    
+  ]
 };
